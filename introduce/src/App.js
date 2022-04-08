e71895010b75b8b092f3ab77a9b57cf13a14388c
BAKHONGMIN <ghdals2655@naver.com>
2022-04-08 10:17:41 +0900

    yarn reate install

---
+++ introduce/src/App.js
@@ -0,0 +1,12 @@
+import Header from './component/Header';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="wrap">
+      <Header className="App-header"></Header>
+    </div>
+  );
+}
+
+export default App;
